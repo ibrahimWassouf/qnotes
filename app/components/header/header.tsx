73f2542297ffe630839f5e9b82a81b3d488fb7dc
Ibrahim Wassouf <ib981347@dal.ca>
2023-05-25 08:56:27 -0300

    Add underline to header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <div className="grid border-b-2 place-items-center mx-auto h-32 w-16 text-center align-middle  md:w-32 lg:w-48">
-        <h1 className="font-sans font-bold text-5xl">QNotes</h1>
+      <div className="grid place-items-center mx-auto h-32 w-16 text-center align-middle  md:w-32 lg:w-48">
+        <h1 className="underline underline-offset-8 decoration-4 decoration-indigo-500 font-sans font-bold text-5xl">
+          QNotes
+        </h1>
       </div>
     </header>
   );
